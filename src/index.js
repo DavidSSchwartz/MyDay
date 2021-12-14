04e4c9f4a0ef150209352342e90e0e27367522f8
David Schwartz <you@example.com>
2021-12-14 15:54:38 +0200

    actually remove all offline plugin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 import "tailwindcss/tailwind.css";
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
-OfflinePluginRuntime.install();
+
 ReactDOM.render(<App />, document.getElementById("root"));
